550c0b07130496906b10db12e1066e8dfda4b0b5
favorable-vector-1743 <epzads@gmail.com>
2019-03-04 16:06:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,13 @@ if array.length  == 1
 
 return array.join 
 
-else if array.length == 2 
+else if array.length == 2
 return array.join( " and ")
 #return 
+
+else if array.length == 3
+  
+  return array[0] + 
 end
 end
 end
